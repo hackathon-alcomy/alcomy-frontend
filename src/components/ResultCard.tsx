c2e10c3567dc4342784d2e46290bc4570a69106b
김부경 <emilly03141221@gmail.com>
2025-11-02 03:14:46 +0900

    (Chore) gif 예시

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -16,11 +16,18 @@ export default function ResultCard({ title, subtitle, id }: Props) {
       }`}
     >
       <div
-        className="h-[96px] w-[96px] rounded bg-[#d9d9d9] hover:shadow-md"
+        className="h-[96px] w-[96px] rounded hover:shadow-md"
         onClick={() => {
           if (id !== undefined) navigate(`/cocktail?id=${id}`);
         }}
-      />
+      >
+        <img
+          src="src\assets\step_4_20251101180400687698.gif"
+          alt="칵테일 흔들리는 이미지"
+          className="h-[96px] w-[96px] rounded object-contain"
+        />
+      </div>
+
       <div className="mt-2 text-ls font-medium">{title}</div>
       {subtitle ? (
         <div className="text-md leading-[1.2] text-[#4b5563]">{subtitle}</div>
