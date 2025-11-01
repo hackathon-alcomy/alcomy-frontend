505e31725dc5e4b8c88998eeaf5b604fbab3918d
김부경 <emilly03141221@gmail.com>
2025-11-01 21:22:40 +0900

    (Feat) 로그인 페이지 퍼블리싱

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
 import logo from "../assets/logo.png";
 import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <header className="h-24 flex items-center justify-between">
       <img src={logo} alt="Alcomy Logo" className="h-8 object-cover" />
-      <Button variant="ghost" className="text-[14px] cursor-pointer">
+      <Button
+        variant="ghost"
+        className="text-[14px] cursor-pointer"
+        onClick={() => navigate("/login")}
+      >
         로그인하기
       </Button>
     </header>
