75966e39d6fc2a4445f8891b667cc3c4daea3f21
김부경 <emilly03141221@gmail.com>
2025-11-01 18:03:59 +0900

    (Chore) 테일윈드 적용, 랜딩 페이지 퍼블

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 import ALCOMYMobile from "./components/ALCOMYMobile";
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    
-    <ALCOMYMobile />
-    
-  )
+function App() {
+  return <ALCOMYMobile />;
 }
 
-export default App
+export default App;
