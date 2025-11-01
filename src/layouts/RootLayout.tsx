e934768ae5eeef2ebb47d3f742f3c1181f2dfefa
김부경 <emilly03141221@gmail.com>
2025-11-02 01:20:16 +0900

    (Feat) 배경 추가

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,13 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import backgroundImage from "../assets/background.png";
 
 export default function RootLayout() {
   return (
-    <div className="min-h-screen mx-auto flex flex-col max-w-[412px] px-2">
-      <Header />
-      <main>
-        <Outlet />
-      </main>
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="bg-background w-full min-h-screen mx-auto flex flex-col max-w-[412px] px-4">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
