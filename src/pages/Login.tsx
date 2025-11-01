9f44db8e2485fe1e04da8f0117be3a478fd47c4f
김부경 <emilly03141221@gmail.com>
2025-11-02 02:03:32 +0900

    (Feat) 로그인 api

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,23 +5,65 @@ import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [id, setId] = useState("");
+  const [mode, setMode] = useState<"signup" | "login">("login");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
     if (!id.trim()) {
       alert("아이디를 입력해주세요!");
       return;
     }
+    if (!password.trim()) {
+      alert("비밀번호를 입력해주세요!");
+      return;
+    }
 
-    localStorage.setItem("userId", id);
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/${mode ? "member" : "auth"}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            nickname: id,
+            password: password,
+          }),
+        }
+      );
 
-    navigate("/my");
+      if (mode === "login") {
+        if (response.status === 200) {
+          localStorage.setItem("userId", id);
+          navigate("/");
+        } else if (response.status === 400) {
+          alert("아이디 또는 비밀번호가 올바르지 않습니다.");
+        }
+      } else if (mode === "signup") {
+        if (response.status === 201) {
+          localStorage.setItem("userId", id);
+          navigate("/");
+        } else if (response.status === 400) {
+          alert("이미 존재하는 아이디입니다.");
+        }
+      }
+    } catch (error) {
+      console.error("Login failed:", error);
+      alert("로그인 중 오류가 발생했습니다.");
+    }
+  };
+
+  const handleSignup = () => {
+    setMode("signup");
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-[80vh] px-6">
-      <h1 className="text-2xl font-semibold mb-6">로그인</h1>
+      <h1 className="text-2xl font-semibold mb-6">
+        {mode === "signup" ? "회원가입" : "로그인"}
+      </h1>
 
       <Input
         type="text"
@@ -38,9 +80,16 @@ export default function Login() {
         className="mb-4"
       />
 
-      <Button className="w-full" onClick={handleLogin}>
-        로그인
-      </Button>
+      <div>
+        <Button className="w-full" onClick={handleLogin}>
+          {mode === "signup" ? "회원가입" : "로그인"}
+        </Button>
+        {mode === "login" && (
+          <Button variant="link" className="w-full" onClick={handleSignup}>
+            회원가입
+          </Button>
+        )}
+      </div>
     </div>
   );
 }
